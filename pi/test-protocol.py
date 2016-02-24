f136229de9989942753b97d9df9e77247eb7ba8b
Daniel Fett <fett@danielfett.de>
2016-02-24 23:25:34 +0100

    Fix some bugs

--- pi/test-protocol.py
+++ pi/test-protocol.py
@@ -1,10 +1,18 @@
+#!/usr/bin/python
+
 import socket
 import hashlib
 import hmac
 import random
 import struct
+import logging
 
-ESP_IP = '127.0.0.1'
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(relativeCreated)6d %(levelname)10s %(name)-20s %(message)s // %(filename)s:%(lineno)s',
+                    )
+
+
+ESP_IP = '10.172.44.91'
 ESP_PORT = 42001
 KEYSET = {
     0: 0xdecafbeddeadbeefcaffeebabe421337,
@@ -19,19 +27,30 @@ class DoorConnection(object):
     BUFFER_SIZE = 64
     
     def __init__(self, ip, port, keyset, getUserChallengeCB):
+        logging.debug("door connection started. ip=%s:%d" % (ip, port))
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.socket.connect((ip, port))
         self.keyset = keyset
         self.getUserChallengeCB = getUserChallengeCB
 
     def send(self, msg):
-        self.sock.send(msg) # ''.join(chr(msg)))
+        l = len(msg)
+        logging.debug("Send message: %r" % msg)
+        sent = self.socket.send(msg) # ''.join(chr(msg)))
+        if sent < l:
+            logging.debug("Only %d of %d bytes sent. Retrying." % (sent, l))
+            
+            #self.send(msg[sent:])
 
     def recv(self, checkMessageIdentifier=None, checkMessageLength=None):
-        inp = self.sock.recv(self.BUFFER_SIZE)
-        if checkMessageIdentifier is not None and inp[0] != checkMessageIdentifier:
+        logging.debug("Waiting for message.")
+        inp = self.socket.recv(self.BUFFER_SIZE)
+        if len(inp) == 0:
+            raise MdosProtocolException("Received empty response.")
+        logging.debug("Received message: %r" % inp)
+        if checkMessageIdentifier is not None and inp[0] != chr(checkMessageIdentifier):
             raise MdosProtocolException(
-                "Expected message identifier %x, received %x!" % (checkMessageIdentifier, inp[0]))
+                "Expected message identifier %x, received %r!" % (checkMessageIdentifier, inp[0]))
         if checkMessageLength is not None and len(inp) != checkMessageLength:
             raise MdosProtocolException(
                 "Expected message length %s, received %s!" % (checkMessageLength, len(inp)))
@@ -41,7 +60,7 @@ class DoorConnection(object):
         return hmac.new(self.keyset[keyid], msg, digestmod=hashlib.sha256).digest()
 
     def toBytes(self, num, bytes=16):
-        s = '%x' % n
+        s = '%x' % num
         if len(s) & 1:
             s = '0' + s
         res = s.decode('hex')
@@ -51,25 +70,32 @@ class DoorConnection(object):
         return self.toBytes(random.SystemRandom())
 
     def login(self, usePresenceChallenge=True):
+        # step 0
+        msg = "\x23"
+        self.send(msg)
+        self.recv(0x42, 1)
+        logging.info("Successfully pinged door unit.")
+        
         # step 1
-        msg = ""
-        "\x00"  # message identifier (MI)
-        "\x42"  # protocol version
+        msg = "\x00\x42"  # message identifier and protocol version
         
         self.send(msg)
 
         # step 2
+        logging.debug("Step 2")
         inp = self.recv(0x01, 17)
         tc = inp[1:16]
 
         # step 3
-        mode = 0x01 if self.usePresenceChallenge else 0x00
+        logging.debug("Step 3")
+        mode = 0x01 if usePresenceChallenge else 0x00
         nc = self.getNonce() # check if this has enough entropy
         mac = self.hmac(tc + mode + nc, 0)
         msg = chr(0x02) + chr(mode) + nc + hmac
         self.send(msg)
     
         # step 4
+        logging.debug("Step 4")
         inp = self.recv(0x03, 17)
         oc = inp[1:16]
 
@@ -79,12 +105,14 @@ class DoorConnection(object):
             pc = "\xff\xff"
 
         # step 5
+        logging.debug("Step 5")
         ac = self.getNonce()
         mac = self.hmac(nc + pc + oc + ac, 1)
 
         self.send("\x04" + ac + mac)
 
         # step 6
+        logging.debug("Step 6")
         inp = self.recv(0x05, 33)
         mac = inp[1:32]
         expmac = self.hmac(ac, 2)
