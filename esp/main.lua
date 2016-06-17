817356b568996d60297bad40719e8c07e3effad9
Guido Schmitz <Guido Schmitz>
2016-06-17 17:54:45 +0200

    worked on main.lua. some refactoring and bug fixing. not tested.

--- esp/main.lua
+++ esp/main.lua
@@ -1,23 +1,55 @@
+-- when this file is reloaded, close running server
 if sv ~= nil then
    sv:close()
 end
 
-gpio.mode(1, gpio.OUTPUT)
-gpio.write(1, gpio.LOW)
 
 
+function first_initialization()
+   debug_output = true
 
-
-K0 = string.char(0xde,0xca,0xfb,0xed,0xde,0xad,0xbe,0xef,0xca,0xff,0xee,0xba,0xbe,0x42,0x13,0x37)
-K1 = string.char(0xbe,0x42,0x13,0x37,0xde,0xca,0xfb,0xed,0xde,0xad,0xbe,0xef,0xca,0xff,0xee,0xba)
-K2 = string.char(0xca,0xff,0xee,0xba,0xbe,0x42,0x13,0x37,0xde,0xca,0xfb,0xed,0xde,0xad,0xbe,0xef)
-KH0 = string.char(0x01,0x23,0x45,0x67,0x89,0x01,0x23,0x46,0x78,0x90,0x12,0x34,0x56,0x78,0x90,0x12)
-KH1 = string.char(0x98,0x76,0x54,0x32,0x10,0x98,0x76,0x54,0x32,0x10,0x98,0x76,0x54,0x32,0x10,0x98)
-
-function tmr_relay_off()
+   gpio.mode(1, gpio.OUTPUT)
    gpio.write(1, gpio.LOW)
-   tmr.unregister(0)
-   disp_off()
+
+   K0 = string.char(0xde,0xca,0xfb,0xed,0xde,0xad,0xbe,0xef,0xca,0xff,0xee,0xba,0xbe,0x42,0x13,0x37)
+   K1 = string.char(0xbe,0x42,0x13,0x37,0xde,0xca,0xfb,0xed,0xde,0xad,0xbe,0xef,0xca,0xff,0xee,0xba)
+   K2 = string.char(0xca,0xff,0xee,0xba,0xbe,0x42,0x13,0x37,0xde,0xca,0xfb,0xed,0xde,0xad,0xbe,0xef)
+   KH0 = string.char(0x01,0x23,0x45,0x67,0x89,0x01,0x23,0x46,0x78,0x90,0x12,0x34,0x56,0x78,0x90,0x12)
+   KH1 = string.char(0x98,0x76,0x54,0x32,0x10,0x98,0x76,0x54,0x32,0x10,0x98,0x76,0x54,0x32,0x10,0x98)
+
+   cnt_rand_insecure = 9999
+   load_cnt_rand_secure()
+
+   state = 0
+   tc = nil
+   nc = nil
+   oc = nil
+   pc = nil
+   mode = nil
+
+end
+
+function printd(s)
+   if debug_output then
+      print(s)
+   end
+end
+
+function tmr_handler()
+   gpio.write(1, gpio.LOW) -- switch off relay
+   tmr.unregister(0) -- reset timer
+   disp_off() -- switch off display
+end
+
+function tmr_start()
+   tmr.register(0, 3000, tmr.ALARM_SINGLE, tmr_handler)
+   tmr.start(0)
+end
+
+function open_door()
+   gpio.write(1, gpio.HIGH)
+   disp_write_open()
+   tmr_start()
 end
 
 function cnt_padding(cnt)
@@ -31,7 +63,6 @@ function cnt_padding(cnt)
    return result
 end
 
-cnt_rand_insecure = 9999
 function rand_insecure()
    result = crypto.encrypt("AES-ECB", KH0, cnt_padding(cnt_rand_insecure))
    cnt_rand_insecure = cnt_rand_insecure + 1
@@ -56,7 +87,6 @@ function load_cnt_rand_secure()
    cnt_rand_secure = tonumber(cnt_rand_secure_str,16) + 128
    save_cnt_rand_secure()
 end
-load_cnt_rand_secure()
 
 
 function rand_secure()
@@ -69,12 +99,6 @@ function rand_secure()
 end
 
 
-state = 0
-tc = nil
-nc = nil
-oc = nil
-pc = nil
-mode = nil
 
 
 --- Returns HEX representation of num
@@ -101,7 +125,7 @@ end
 
 
 function disc(c)
-   print ("disconnected")
+   printd ("disconnected")
    state = 0
    tc = nil
    nc = nil
@@ -110,93 +134,103 @@ function disc(c)
    mode = nil
 end
 
-function recv(c, pl)
-   local ip, port = c:getpeer()
-   print ("data from " .. ip)
-   msg = nil
-   print ("received " .. pl:len() .. " bytes: " .. explode_string(pl) .. " string: " .. pl)
-   if pl:len() == 1 and pl:byte(1) == 0x23 then
-      print "PING/PONG"
-      msg = string.char(0x42)
-      -- c:send(string.char(0x42))
-   elseif state == 0 and pl:len() == 2 and pl:byte(1) == 0x0 and pl:byte(2) == 0x42 then
-      print "0:"
-      msgid = string.char(0x01)
-      tc = rand_insecure()
-      print ("tc " .. explode_string(tc))
-      msg = msgid .. tc
-      -- c:send(msg)
+
+function process_pl_0(pl)
+   printd "0:"
+   local msgid = string.char(0x01)
+   tc = rand_insecure()
+   printd ("tc " .. explode_string(tc))
+   local msg = msgid .. tc
+   state = state + 1
+   return msg
+end
+
+
+function process_pl_2(pl)
+   printd "2:"
+   local mode = pl:sub(2,2)
+   nc = pl:sub(3,18)
+   local h_rec = pl:sub(19,50)
+   local h_own = crypto.hmac("sha256", tc .. mode .. nc, K0)
+   printd ("mode " .. explode_string(mode))
+   printd ("nc " .. explode_string(nc))
+   printd ("h_rec " .. explode_string(h_rec))
+   printd ("h_own " .. explode_string(h_own))
+   if h_rec == h_own then
+      local msgid = string.char(0x03)
+      oc = rand_secure()
+      printd ("oc " .. explode_string(oc))
+      if mode == string.char(0x01) then
+	 local pc_src = rand_secure()
+	 local pc_num = (pc_src:byte(1) * 256 + pc_src:byte(2)) % 10000
+	 disp_write_num_reverse(pc_num)
+	 pc = ""
+	 for i=1,4,1 do
+	    pc = pc .. string.char(pc_num % 10)
+	    pc_num = math.floor(pc_num / 10)
+	 end     
+      else
+	 -- CURRENTLY DISABLED: mode w/o presence challenge
+	 pc = string.char(0xff) .. string.char(0xff) .. string.char(0xff) .. string.char(0xff)
+	 return nil
+      end
+      printd ("pc " .. explode_string(pc))
+      local msg = msgid .. oc
       state = state + 1
+      return msg
+   end
+   return nil
+end
+
+function process_pl_4(pl)
+   printd "4:"
+   ac = pl:sub(2,17)
+   local h_rec = pl:sub(18,49)
+   local h_own = crypto.hmac("sha256", nc .. pc .. oc .. ac, K1)
+   printd ("ac " .. explode_string(ac))
+   printd ("h_rec " .. explode_string(h_rec))
+   printd ("h_own " .. explode_string(h_own))
+   if h_rec == h_own then
+      printd ("OK")
+      open_door()
+      --tmr.delay(4000000)
+      --gpio.write(1, gpio.LOW)
+      local msgid = string.char(0x05)
+      local h = crypto.hmac("sha256", ac, K2)
+      local msg = msgid .. h
+      state = 0
+      return msg
+   end
+   return nil
+end
+
+function recv(c, pl)
+   local msg = nil
+   local ip, port = c:getpeer()
+   printd ("data from " .. ip)
+   local msg = nil
+   printd ("received " .. pl:len() .. " bytes: " .. explode_string(pl) .. " string: " .. pl)
+   if pl:len() == 1 and pl:byte(1) == 0x23 then
+      printd "PING/PONG"
+      msg = string.char(0x42)
+   elseif state == 0 and pl:len() == 2 and pl:byte(1) == 0x0 and pl:byte(2) == 0x42 then
+      msg = process_pl_0(pl)
    elseif state == 1 and pl:len() == 50 and pl:byte(1) == 0x2 then
-      print "2:"
-      mode = pl:sub(2,2)
-      nc = pl:sub(3,18)
-      h_rec = pl:sub(19,50)
-      h_own = crypto.hmac("sha256", tc .. mode .. nc, K0)
-      print ("mode " .. explode_string(mode))
-      print ("nc " .. explode_string(nc))
-      print ("h_rec " .. explode_string(h_rec))
-      print ("h_own " .. explode_string(h_own))
-      if h_rec == h_own then
-	 msgid = string.char(0x03)
-	 oc = rand_secure()
-	 print ("oc " .. explode_string(oc))
-	 if mode == string.char(0x01) then
-	    local pc_src = rand_secure()
-	    local pc_num = (pc_src:byte(1) * 256 + pc_src:byte(2)) % 10000
-        disp_write_num_reverse(pc_num)
-	    pc = ""
-	    for i=1,4,1 do
-	       pc = pc .. string.char(pc_num % 10)
-	       pc_num = math.floor(pc_num / 10)
-	       --pc = string.char(pc_src:byte(1) % 10) .. string.char(pc_src:byte(2) % 10) .. string.char(pc_src:byte(3) % 10) .. string.char(pc_src:byte(4) % 10)
-	    end     
-	 else
-	    pc = string.char(0xff) .. string.char(0xff) .. string.char(0xff) .. string.char(0xff)
-	 end
-	 print ("pc " .. explode_string(pc))
-	 msg = msgid .. oc
-	 -- c:send(msg)
-	 state = state + 1
-      else
-	 c:close()
-   disp_write_fail()
-      end
+      msg = process_pl_2(pl)
    elseif state == 2 and pl:len() == 49 and pl:byte(1) == 0x4 then
-      print "4:"
-      ac = pl:sub(2,17)
-      h_rec = pl:sub(18,49)
-      h_own = crypto.hmac("sha256", nc .. pc .. oc .. ac, K1)
-      print ("ac " .. explode_string(ac))
-      print ("h_rec " .. explode_string(h_rec))
-      print ("h_own " .. explode_string(h_own))
-      if h_rec == h_own then
-	 print ("OK")
-	 gpio.write(1, gpio.HIGH)
-    disp_write_open()
-	 tmr.register(0, 3000, tmr.ALARM_SINGLE, tmr_relay_off)
-	 tmr.start(0)
-	 --tmr.delay(4000000)
-	 --gpio.write(1, gpio.LOW)
-	 msgid = string.char(0x05)
-	 h = crypto.hmac("sha256", ac, K2)
-	 msg = msgid .. h
-	 -- c:send(msg)
-	 state = 0
-      else
-	 c:close()
-   disp_write_fail()
-      end
+      msg = process_pl_4(pl)
+   end
+   if msg ~= nil then
+      printd ("send " .. msg:len() .. " bytes: " .. explode_string(msg) .. " string: " .. msg)
+      c:send(msg)
    else
       c:close()
       disp_write_fail()
-   end
-   if msg ~= nil then
-      print ("send " .. msg:len() .. " bytes: " .. explode_string(msg) .. " string: " .. msg)
-      c:send(msg)
+      tmr_start()
    end
 end
 
+first_initialization()
 
 sv = net.createServer(net.TCP, 30)
 
