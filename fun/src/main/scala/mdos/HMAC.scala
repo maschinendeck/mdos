3bf7efac3b27e393031cd7ddced024971efbc30c
Stefan Ollinger <Stefan.Ollinger@gmx.de>
2016-10-30 23:56:11 +0100

    challenge contains random and time component

--- fun/src/main/scala/mdos/HMAC.scala
+++ fun/src/main/scala/mdos/HMAC.scala
@@ -8,12 +8,20 @@ object HMAC {
 
   def random: BitVector = BitVector(scala.util.Random.nextInt())
 
-  val lim = BitVector("|".getBytes)
+  def concat(xs: BitVector*): BitVector = {
+    xs.toList match {
+      case x :: Nil => x
+      case y :: ys => y ++ delim ++ concat(ys:_*)
+      case Nil => BitVector.empty
+    }
+  }
+
+  lazy val delim = BitVector("|".getBytes)
 
   def sha256 = MessageDigest.getInstance("SHA-256")
 
   def sign(value: BitVector, secret: BitVector): BitVector = {
-    BitVector(sha256.digest((value ++ lim ++ secret).toByteArray))
+    BitVector(sha256.digest(concat(secret, value).toByteArray))
   }
 
   def check(sig: BitVector, value: BitVector, secret: BitVector): Boolean = {
---
+++ fun/src/main/scala/mdos/door3.scala
@@ -0,0 +1,184 @@
+package mdos
+
+import fs2._
+import scodec.bits._
+
+// experiment of a protocol version
+// - no presence challenge
+// - no acknowledge
+// - no protocol configuration
+// - challenge contains time component, refuse sign requests out of time window, prevent
+object door3 extends App {
+
+  implicit val executorStrategy = fs2.Strategy.fromFixedDaemonPool(2)
+
+
+  val K0 = hex"decafbeddeadbeefcaffeebabe421337".bits
+  val K1 = hex"be421337decafbeddeadbeefcaffeeba".bits
+  val K2 = hex"caffeebabe421337decafbeddeadbeef".bits
+
+
+
+  sealed trait Msg
+
+  object Msg {
+
+    // messages to door
+    case class Trigger(protocolVersion: Byte, time: Long) extends Msg
+    case class Open(hmac: BitVector) extends Msg
+
+    // messages from door
+    case class OpeningChallenge(openingChallenge: BitVector, time: Long) extends Msg
+
+  }
+
+
+  sealed trait DoorState
+
+  object DoorState {
+    case object Idle extends DoorState
+    case object WaitForOpen extends DoorState
+  }
+
+
+
+
+  val doorState = fs2.async.signalOf[Task, DoorState](DoorState.Idle).unsafeRun()
+
+  // opening challenge register, prevent replay attacks by adding a random number
+  val doorOC = fs2.async.signalOf[Task, Option[(BitVector, Long)]](None).unsafeRun()
+
+  val doorReceive = fs2.async.unboundedQueue[Task, Msg].unsafeRun()
+
+  val publicReceive = fs2.async.unboundedQueue[Task, Msg].unsafeRun()
+
+
+
+
+  object Door {
+
+    def idle: Task[Unit] = {
+      for {
+        _ <- Task {
+          println("reset registers, go to Idle state")
+        }
+        _ <- doorState.set(DoorState.Idle)
+        _ <- doorOC.set(None)
+      } yield ()
+    }
+
+    def openingChallenge(time: Long): Task[Unit] = {
+      for {
+        _ <- Task {
+          println("send opening challenge and go to WaitForOpen state")
+        }
+        _ <- doorState.set(DoorState.WaitForOpen)
+        random = HMAC.random
+        time = System.currentTimeMillis
+        _ <- doorOC.set(Some(random, time))
+        _ <- publicReceive.offer1(Msg.OpeningChallenge(random, time))
+      } yield ()
+    }
+
+    def abort: Task[Unit] = {
+      Task(println("abort")) flatMap (_ => idle)
+    }
+
+    def open: Task[Unit] = {
+      Task(println("opening door")) flatMap (_ => idle)
+    }
+
+  }
+
+  object Public {
+
+    def sendOpen(openingChallenge: BitVector, time: Long): Task[Unit] = {
+      val sig = HMAC.sign(HMAC.concat(openingChallenge, BitVector(time)), K0)
+      doorReceive.offer1(Msg.Open(sig)).map(_ => ())
+    }
+
+  }
+
+
+
+  val door: Stream[Task, Unit] = {
+
+    ((doorReceive.dequeue zip doorState.continuous) zip doorOC.continuous).evalMap[Task, Task, Unit] {
+      case ((msg, state), random1) =>
+        println(s"(door) state: $state random1: $random1 received: $msg")
+
+        state match {
+          case DoorState.Idle =>
+            msg match {
+              case Msg.Trigger(0x43, time) => Door.openingChallenge(time)
+              case _ => Door.abort
+            }
+
+          case DoorState.WaitForOpen =>
+            (msg, random1) match {
+              case (Msg.Open(sig), Some((r1, time))) =>
+                if (HMAC.check(sig, HMAC.concat(r1, BitVector(time)), K0)) Door.open
+                else Door.abort
+
+              case _ => Door.abort
+            }
+
+        }
+
+    }
+
+  }
+
+  val public = {
+
+    for {
+      msg <- publicReceive.dequeue
+      _ <- Stream.eval[Task, Any] {
+        println(s"(public) msg: $msg")
+
+        msg match {
+          case Msg.OpeningChallenge(challenge, time) =>
+
+            val now = System.currentTimeMillis()
+            if (math.abs(time - now) < 3600 * 1000) {
+              Public.sendOpen(challenge, time)
+            } else {
+              println("msg out of time window, ignoring")
+              Task.now(())
+            }
+
+          case _ => Task.now(())
+        }
+
+      }
+    } yield ()
+
+  }
+
+
+  // start asynchronous processes
+
+  door.run.unsafeRunAsync(_ => ())
+  public.run.unsafeRunAsync(_ => ())
+
+
+
+  // simulate some messages
+
+  println("initialize")
+  doorReceive.offer1(Msg.Trigger(0x43, System.currentTimeMillis())).unsafeRun()
+  scala.io.StdIn.readLine
+
+
+
+  println("refuse Open without prior start")
+  doorReceive.offer1(Msg.Open(BitVector.empty)).unsafeRun()
+  scala.io.StdIn.readLine
+
+
+  println("refuse Msg.OpeningChallenge(challenge, time) out of time range")
+  publicReceive.offer1(Msg.OpeningChallenge(BitVector.empty, 0)).unsafeRun()
+  scala.io.StdIn.readLine
+
+
+}
